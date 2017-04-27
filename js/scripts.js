0e4cea2433f1fb75c1caef5a2bd71131e2693859
Ian Garner, Jenna Cooper and Roger Tobias <ian+jenna+roger@>
2017-04-27 13:59:32 -0700

    will get logic tp flow properly and debugged any errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
   var height = parseInt(prompt("How tall are you in inches?"));
 
-  if (height >= 53) {
-    $('.just-right').show();
-  }
-  if else (height > 83) {
+  if (height <= 53) {
+    $('.too-short').show();
+  } else if (height > 83) {
     $('.too-tall').show();
-  }
-  else  {
-    $('too-short').show();
+  } else  {
+    debugger;
+    $('.just-right').show();
   }
 });
